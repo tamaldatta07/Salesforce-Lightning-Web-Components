779d974148831ccbdac82cb4cbcc244881762e80
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2024-07-01 16:31:19 +0530

    Conditional Rendering Demo

--- force-app/main/default/lwc/conditionalRenderingDemo/conditionalRenderingDemo.js
+++ force-app/main/default/lwc/conditionalRenderingDemo/conditionalRenderingDemo.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class ConditionalRenderingDemo extends LightningElement {}
+export default class ConditionalRenderingDemo extends LightningElement {
+    @track onClickedButtonLabel = 'Show';
+    myTitle = 'Conditional Rendering Demo';
+    @track cardVisible = false;
+
+    handleClick(event){
+        const label = event.target.label
+
+        if(label === 'Show'){
+            this.onClickedButtonLabel = 'Hide';
+            this.cardVisible = true;
+        }
+        else if(label === 'Hide'){
+            this.onClickedButtonLabel = 'Show';
+            this.cardVisible = false;
+
+        }
+    }
+}
